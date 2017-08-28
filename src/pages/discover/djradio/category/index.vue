41f40d31d12abb3d2dcb854b6a99d3a41c4f87b0
Qiming <sandmanman@qq.com>
2017-08-28 13:25:08 +0800

    电台分类=>优秀新电台

--- src/pages/discover/djradio/category/index.vue
+++ src/pages/discover/djradio/category/index.vue
@@ -1,5 +1,22 @@
 <template>
-    <h1>分类</h1>
+    <div>
+        <div class="new" v-if="hasQueryId">
+            <div class="u-title f-cb">
+                <h3><span class="f-ff2">优秀新电台</span></h3>
+            </div>
+            <ul class="m-rdilist f-cb" v-if="djRecommendData">
+                <li v-for="item in djRecommendData" :key="item.id">
+                    <a :href="'/djradio?id='+item.id" class="u-cover u-cover-rdi2">
+                        <img :src="item.picUrl+'?param=200y200'" alt="">
+                    </a>
+                    <h3 class="f-fs2">
+                        <a :href="'/djradio?id='+item.id" class="s-fc1" :title="item.name">{{item.name}}</a>
+                    </h3>
+                    <p class="f-thide2 s-fc4">{{item.rcmdtext}}</p>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,15 +25,26 @@ export default {
     name: 'Category',
     data() {
         return {
-            djData: null,
+            djRecommendData: null,
+            hasQueryId: true
         }
     },
     created() {
-
+        if ( !this.$route.query.id ) {
+            this.hasQueryId = !this.hasQueryId
+        } else {
+            this.hasQueryId = true
+            this.getDJRecommendType(this.$route.query.id)
+        }
     },
     watch: {
         '$route'(to, from, next) {
-            this.getDJRecommendType(this.$route.query.id)
+            if ( !this.$route.query.id ) {
+                this.hasQueryId = !this.hasQueryId
+            } else {
+                this.hasQueryId = true
+                this.getDJRecommendType(this.$route.query.id)
+            }   
         }
     },
     methods: {
@@ -24,7 +52,7 @@ export default {
             // 分类推荐
             djRecommendType(id).then(res => {
                 if ( res.data.code === 200 ) {
-                    this.djData = res.data.djRadios
+                    this.djRecommendData = res.data.djRadios.splice(0,5)
                 } else {
                     console.error('数据获取出错了')
                 }
